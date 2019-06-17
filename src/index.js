9d2939b44add35ad73cdadcd828f4c1e356d72c8
asad <asadrajput2@gmail.com>
2019-06-17 21:43:48 +0530

    Initial comit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import ToDoList from './todolist'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Container extends React.Component {
+  render() {
+    return (
+      <div class="container">
+        <ToDoList/>
+      </div>
+    )
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Container />,
+  document.getElementById('root')
+)
---
+++ src/todolist.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import './index.css'
+
+class ToDoList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      list_arr: []
+    }
+    this.addItem = this.addItem.bind(this)
+    this.removeItem = this.removeItem.bind(this)
+    
+  }
+
+  addItem(e) {
+    e.preventDefault()
+    this.setState( 
+      function (state) {
+        if (this.input.value) {
+          state.list_arr.unshift(this.input.value)
+          this.input.value =""
+        }
+        const list_arr = state.list_arr
+        return { list_arr: list_arr }
+      }
+    )
+  }
+
+  removeItem(i) {
+    this.setState(
+      (state) => {
+        state.list_arr.splice(i, 1)
+        const list_arr = state.list_arr
+        return { list_arr: list_arr }
+      }
+    )
+  }
+
+  render() {  
+    return (
+      <div>
+        <form class="form" onSubmit={this.addItem}>
+          <input type="text" ref={(input) => { this.input = input }}
+                  placeholder="add item" />
+          <button type="submit">Add</button>
+        </form>
+        <div class="list">
+          {this.state.list_arr.map(
+            (a, i, arr) =>
+              <div key={i} class="item">
+                {a}
+                <div class="close"
+                      onClick={(e) => this.removeItem(i)}>
+                  &times;
+                </div>
+              </div>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default ToDoList;  
