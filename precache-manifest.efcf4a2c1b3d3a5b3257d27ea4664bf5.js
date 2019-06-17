9d950f054fb5a10ff9f822864bfcb3ef7084f7d9
asad <asadrajput2@gmail.com>
2019-06-17 21:47:30 +0530

    Updates

---
+++ precache-manifest.efcf4a2c1b3d3a5b3257d27ea4664bf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60a15a4ce8bbae603fdc747fa8ff463",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "748bd4daae5e2b723454",
+    "url": "/todolist/static/css/main.8e12824e.chunk.css"
+  },
+  {
+    "revision": "a36533ae65938741f383",
+    "url": "/todolist/static/js/2.4f017e5b.chunk.js"
+  },
+  {
+    "revision": "748bd4daae5e2b723454",
+    "url": "/todolist/static/js/main.553f9080.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
